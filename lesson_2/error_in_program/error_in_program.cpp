b6388a1709ec70ad7ece1437cca9956b206c6e36
Sergey Ovsyannikov <ovssv87@gmail.com>
2024-03-26 21:52:40 +0400

    initial commit

---
+++ lesson_2/error_in_program/error_in_program.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Point {
+	double m_x;
+	double m_y;
+	Point(double x, double y) {
+		m_x = x;
+		m_y = y;
+	}
+};
+
+void print_point(const Point& point_object) {
+	cout << "x:" << point_object.m_x << ", y: " << point_object.m_y << endl;
+}
+
+int main()
+{
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		Point my_point(i, 2 * i);
+		print_point(my_point);
+	}
+
+	return 0;
+}
