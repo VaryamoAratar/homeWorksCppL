725ae267b33c103206279d9b5b6aa9ad8adfebbe
Sergey Ovsyannikov <ovssv87@gmail.com>
2024-03-30 18:18:38 +0400

    Created lesson 1

---
+++ lesson_1/lambda_func/main.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+
+
+
+int main()
+{
+	std::vector<int> v{ 4, 7, 9, 14, 12 };
+	for (auto i : v)
+		std::cout << i << ' ';
+
+	std::for_each(v.begin(), v.end(),
+		[](int& n) { if (n % 2)	n *= 3; });
+
+	for (auto i : v)
+		std::cout << i << ' ';
+
+	return 0;
+}
