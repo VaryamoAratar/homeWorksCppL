520ad85813e8f501536a0fb6deaef137583cac8e
Sergey Ovsyannikov <ovssv87@gmail.com>
2024-04-21 17:50:59 +0400

    Created template_sqr

---
+++ lesson_5/template_sqr/template_sqr.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+
+
+template<class T>
+T mySqr(T x)
+{
+	return x * x;
+}
+
+template<class T>
+std::vector<T> mySqr(std::vector<T> x)
+{
+	for (auto& i : x)
+	{
+		i *= i;
+	}
+
+	return x;
+}
+
+
+int main()
+{
+
+	int in = 4;
+
+	std::cout << "[IN]: " << in << "\n[OUT]: " << mySqr(in) << std::endl;
+
+	std::vector<double> inVector{ -1.2, 4, 8.2 };
+	std::cout << "[IN]: ";
+	for (auto i : inVector)
+		std::cout << i << ' ';
+	std::cout << "\n[OUT]: ";
+	for (auto i : mySqr(inVector))
+		std::cout << i << ' ';
+
+	return 0;
+}
