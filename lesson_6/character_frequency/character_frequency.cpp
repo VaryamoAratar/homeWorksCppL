cbe1a57ac02dbabbc351d6f85fd3ab116ffb7005
Sergey Ovsyannikov <ovssv87@gmail.com>
2024-04-22 17:03:46 +0400

    Added comments

--- lesson_6/character_frequency/character_frequency.cpp
+++ lesson_6/character_frequency/character_frequency.cpp
@@ -1,36 +1,55 @@
 ﻿#include <iostream>
-#include <unordered_set>
 #include <set>
-#include <map>
 #include <string>
 #include <exception>
 
-struct MyStruct
+struct CountedСharacters
 {
-	char val;
-	int count;
+public:
+	CountedСharacters(char val, int count) : _val(val), _count(count) {};
+	int _count;
+	char _val;
+
+	bool operator < (CountedСharacters s) const
+	{
+		if (s._count == _count)
+			return s._val < _val;
+		return s._count < _count;
+	}
 };
 
 int main(int argc, char* argv[])
 {
 	try
 	{
+		//объявляем строку для чтения с консоли
 		std::string str_in;
 		std::cout << "[IN]: ";
+		//запрашиваем строку у пользователя
 		std::getline(std::cin, str_in);
-		std::unordered_multiset<char> symbol;
+		//Перегоняем строку в контейнер char'ов
+		std::multiset<char> symbol;
 		for (auto i = 0; i < str_in.length(); i++)
 		{
 			symbol.insert(str_in[i]);
 		}
+		//Проверяем заполнение контейнера
 		if (symbol.empty())
 		{
 			throw std::runtime_error("Multyset is Empty!");
 		}
-
-		for (auto i = symbol.begin(); i!= symbol.end(); i++)
+		//объявляем контейнер посчитаннх символов
+		std::set<CountedСharacters> сounted_сharacters;
+			std::cout << "[OUT]: " << std::endl;
+		//перегоняем символы с подсчитанным числом, отсеиваем дубликаты
+		for (auto i : symbol)
 		{
-			std::cout << *i << ' ' << symbol.count(*i) << std::endl;
+			сounted_сharacters.insert(CountedСharacters(i, symbol.count(i)));
+		}
+		//выводим результат
+		for (auto i = сounted_сharacters.begin(); i != сounted_сharacters.end(); i++)
+		{
+			std::cout << i->_val << ' ' << i->_count << std::endl;
 		}
 	}
 	catch (const std::exception& ex)
