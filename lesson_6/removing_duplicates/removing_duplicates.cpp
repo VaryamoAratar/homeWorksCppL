84cc793c59cbfe05e28f2869801130a2ff586e9a
Sergey Ovsyannikov <ovssv87@gmail.com>
2024-04-22 17:16:51 +0400

    Created task 2 in lesson 6

---
+++ lesson_6/removing_duplicates/removing_duplicates.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <set>
+#include <exception>
+
+int main(int argc, char* argv[])
+{
+	try
+	{
+		std::cout << "[IN]: " << std::endl;
+		//запрашиваем у пользователя количество элементов
+		int num = 0;
+		std::cin >> num;
+
+		//объявляем контейнер для удаления дубликатов
+		std::set<int> remov_dupl;
+
+		//вводим значения
+		for (auto i = 0; i < num; i++)
+		{
+			int in;
+			std::cin >> in;
+			remov_dupl.insert(in);
+		}
+		//проверяем заполнение контейнера
+		if (remov_dupl.empty())
+		{
+			throw std::runtime_error("remov_dupl is Empty!");
+		}
+		std::cout << "[OUT]: " << std::endl;
+		//выводим результат
+		for (auto i = remov_dupl.rbegin(); i != remov_dupl.rend(); i++)
+		{
+			std::cout << *i << std::endl;
+		}
+	}
+	catch (const std::exception& ex)
+	{
+		std::cerr << ex.what() << std::endl;
+	}
+	catch (...)
+	{
+		std::cerr << "invalid value" << std::endl;
+	}
+	return 0;
+}
