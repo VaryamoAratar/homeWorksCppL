2c9a6de4194541635c823f02e64228d4deda46c4
Sergey Ovsyannikov <ovssv87@gmail.com>
2024-04-22 23:53:08 +0400

    Created lesson 7

---
+++ lesson_7/print/print.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+#include <list>
+#include <vector>
+
+template<class T>
+void print_container(const T& cont)
+{
+    for (auto i : cont)
+    {
+        std::cout << i << ' ';
+    }
+};
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+
+    print_container<std::set<std::string>>(test_set);
+    std::cout << std::endl;
+    print_container<std::list<std::string>>(test_list);
+    std::cout << std::endl;
+    print_container<std::vector<std::string>>(test_vector);
+
+    return 0;
+}
+
---
+++ lesson_7/unique/unique.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+    std::vector<int> v = { 1, 1, 2, 5, 6, 1, 2, 4 };
+    std::cout << "[IN]: ";
+    for (auto i : v)
+    {
+        std::cout << i << ' ';
+    }
+    std::cout << "\n[OUT]: ";
+
+    std::sort(v.begin(), v.end());
+    auto itr = std::unique(v.begin(), v.end());
+    v.erase(itr, v.end());
+    for (auto i : v)
+    {
+        std::cout << i << ' ';
+    }
+    return 0;
+}
+
