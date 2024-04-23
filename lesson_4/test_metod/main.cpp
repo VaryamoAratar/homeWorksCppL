9e5df5f0721d713916fdc222aef5ca70cfebddc7
Sergey Ovsyannikov <ovssv87@gmail.com>
2024-04-23 22:05:27 +0400

    Udated test_2

---
+++ lesson_4/test_metod/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <List.h>
+
+int main()
+{
+	std::cout << "Hello" << std::endl;
+	List _list;
+	_list.PushBack(5);
+
+}
--- lesson_4/test_metod/main_test.cpp
+++ lesson_4/test_metod/main_test.cpp
@@ -1,125 +1,90 @@
-#include <catch2/catch_test_macros.hpp>
+#include <catch.hpp>
+#include <cstdint>
+#include <List.h>
 
-#include <List.cpp>
-
-TEST_CASE("PopFront and PopBack")
+TEST_CASE("Check PushBack", "[List]")
 {
-    SECTION("Получаем значения с пустого списка")
-    {
-        List test_1;
-
-        try
-        {
-            std::cout << "Удачное извлечение спереди списка, значение: " << test_1.PopFront() << std::endl;
-        }
-        catch (...)
-        {
-            std::cerr << "Невозможно извлечь значение спереди списка. Размер списка: " << test_1.Size() << std::endl;
-        }
-        try
-        {
-            std::cout << "Удачное извлечение сзади списка, значение: " << test_1.PopBack() << std::endl;
-        }
-        catch (...)
-        {
-            std::cerr << "Невозможно извлечь значение сзади списка. Размер списка: " << test_1.Size() << std::endl;
-        }
-
-    }
-    SECTION("Получаем значения со списка из 5 значений")
-    {
-        List test_2;
-        for (size_t i = 0; i < 5; i++)
-        {
-            test_2.PushBack(i + 1);
-        }
-        try
-        {
-            std::cout << "Удачное извлечение спереди списка, значение: " << test_2.PopFront() << std::endl;
-        }
-        catch (...)
-        {
-            std::cerr << "Невозможно извлечь значение спереди списка. Размер списка: " << test_2.Size() << std::endl;
-        }
-        try
-        {
-            std::cout << "Удачное извлечение сзади списка, значение: " << test_2.PopBack() << std::endl;
-        }
-        catch (...)
-        {
-            std::cerr << "Невозможно извлечь значение сзади списка. Размер списка: " << test_2.Size() << std::endl;
-        }
-
-    }
+	List test_1;
+	test_1.PushBack(1);
+	REQUIRE(test_1.Size() == 1);
 }
-TEST_CASE("PushFront")
+TEST_CASE("CHeck PushFront", "[List]")
 {
-    SECTION("Добавление спереди")
-    {
-        List test_2;
-        for (size_t i = 0; i < 5; i++)
-        {
-            test_2.PushFront(i + 1);
-        }
-        try
-        {
-            for (size_t i = 5; i > 0; i--)
-            {
-                REQUIRE(test_2.PopFront() == i);
-            }
-        }
-        catch (...)
-        {
-            std::cerr << "Ошибка извлечения значение спереди списка" << std::endl;
-        }
-
-    }
+	List test_2;
+	test_2.PushFront(-1);
+	REQUIRE(test_2.Size() == 1);
 }
-TEST_CASE("PushBack")
+TEST_CASE("Chech PopBack", "[List]")
 {
-    SECTION("Добавление сзади")
-    {
-        List test_3;
-        for (size_t i = 0; i < 5; i++)
-        {
-            test_3.PushBack(i + 1);
-        }
-        try
-        {
-            for (size_t i = 5; i > 0; i--)
-            {
-                REQUIRE(test_3.PopBack() == i);
-            }
-        }
-        catch (...)
-        {
-            std::cerr << "Ошибка извлечения значение сзади списка" << std::endl;
-        }
-
-    }
+	List test_3;
+	test_3.PushBack(1);
+	test_3.PushBack(2);
+	test_3.PushBack(3);
+	REQUIRE(test_3.PopBack() == 3);
 }
-TEST_CASE_METHOD(List, "Scenario")
+TEST_CASE("Check PopFront", "[List]")
 {
-    List test_4;
-    for (size_t i = 0; i < 5; i++)
-    {
-        test_4.PushBack(i + 1);
-        test_4.PushFront((i + 1) * (-1));
-    }
-    try
-    {
-        REQUIRE(test_4.PopBack() == 5);
-        REQUIRE(test_4.PopFront() == -5);
-        test_4.PushFront(-6);
-        REQUIRE(test_4.PopFront() == -6);
-        REQUIRE(test_4.PopBack() == 4);
-        REQUIRE(test_4.PopBack() == 3);
-        test_4.PushBack(6);
-        REQUIRE(test_4.PopBack() == 6);
-    }
-    catch (...)
-    {
-        std::cerr << "Ошибка извлечения значения" << std::endl;
-    }
+	List test_4;
+	test_4.PushBack(1);
+	test_4.PushBack(2);
+	test_4.PushBack(3);
+	REQUIRE(test_4.PopFront() == 1);
+}
+SCENARIO("Multiple method calls", "[List]")
+{
+	GIVEN("Creating an empty list and adding three items")
+	{
+		List list_test;
 
+		REQUIRE(list_test.Empty() == true);
+		REQUIRE(list_test.Size() == 0);
+
+		WHEN("Extracting an item from an empty list")
+		{
+			REQUIRE_THROWS(list_test.PopBack() == true);
+			REQUIRE_THROWS(list_test.PopFront() == true);
+		}
+
+		list_test.PushBack(-1);
+		list_test.PushBack(0);
+		list_test.PushBack(1);
+
+		REQUIRE(list_test.Empty() == false);
+		REQUIRE(list_test.Size() == 3);
+
+		WHEN("Adding an element to the end")
+		{
+			list_test.PushBack(2);
+			THEN("the size should increase")
+			{
+				REQUIRE(list_test.Size() == 4);
+			}
+		}
+		WHEN("Adding an element to the front")
+		{
+			list_test.PushFront(-2);
+			THEN("the size should increase")
+			{
+				REQUIRE(list_test.Size() == 4);
+			}
+		}
+		WHEN("Extracting the element from the end")
+		{
+			int t = list_test.PopBack();
+			THEN("The value of the element is 1 and the size should decrease")
+			{
+				REQUIRE(t == 1);
+				REQUIRE(list_test.Size() == 2);
+			}
+		}
+		WHEN("Extracting the element from the front")
+		{
+			int t = list_test.PopFront();
+			THEN("The value of the element is -1 and the size should decrease")
+			{
+				REQUIRE(t == -1);
+				REQUIRE(list_test.Size() == 2);
+			}
+		}
+	}
 }
