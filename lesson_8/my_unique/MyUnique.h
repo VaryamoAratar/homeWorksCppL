c5e798ef58f568963bc5bfceca009f57baa38bd0
Sergey Ovsyannikov <ovssv87@gmail.com>
2024-04-24 16:39:35 +0400

    Created MyUnique

---
+++ lesson_8/my_unique/MyUnique.h
@@ -0,0 +1,35 @@
+#pragma once
+
+template<class T>
+class MyUnique
+{
+private:
+	T* _ptr = new T;
+public:
+	MyUnique(T* ptr)
+	{
+		_ptr = ptr;
+	};
+	~MyUnique()
+	{
+		std::cout << "Call destructor" << std::endl;
+		delete _ptr;
+	};
+
+	MyUnique(const MyUnique& copy) = delete;
+	MyUnique& operator = (const MyUnique& copy) = delete;
+
+	T operator * ()
+	{
+		return *_ptr;
+	};
+
+	T* release()
+	{
+		delete _ptr;
+
+		_ptr = nullptr;
+		return _ptr;
+	}
+};
+
---
+++ lesson_8/my_unique/main.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "MyUnique.h"
+
+int main()
+{   
+    MyUnique<int> ptr(new int(5));
+    std::cout << *ptr << std::endl;
+    ptr.release();
+
+    return 0;
+}
